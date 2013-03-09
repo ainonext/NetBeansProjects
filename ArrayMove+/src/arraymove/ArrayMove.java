26cd23852ccc461a29a9a992f353a59d521bb480
StoryD <sm_sv_ma@mail.ru>
2013-03-09 23:22:07 +0400

    array shift to the right

--- ArrayMove+/src/arraymove/ArrayMove.java
+++ ArrayMove+/src/arraymove/ArrayMove.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ArrayMove {
 
     public static void main(String[] args) {
+        //Нахождение максимального, минимального элемента и среднего арифметического 
         Scanner in = new Scanner(System.in);
         int min;
         int max;
@@ -27,5 +28,31 @@ public class ArrayMove {
         }
         System.out.println("Минимум " + min + "\nМаксимум " + max);
         System.out.println("Среднее арифметическое " + (s - (min + max)) / n);
+        //Сдвижка массива на заданное количество позиций
+        System.out.println("Введите размерность массива");
+        int num = in.nextInt();
+        int[] array = new int[num];
+        System.out.println("Введите шаг для сдвига ");
+        int k = in.nextInt();
+        System.out.println("Исходный массив ");
+        for (int i = 0; i < num; i++) {
+            array[i] = (int) (Math.random() * 5 * num);
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("Массив после сдвига");
+        int prev = array[0];
+        int t, step = 0;
+        do {
+            for (int i = 1; i < num; i++) {
+                t = array[i];
+                array[i] = prev;
+                prev = t;
+            }
+            array[0] = prev;
+            step++;
+        } while (step != k);
+        for (int i = 0; i < num; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
