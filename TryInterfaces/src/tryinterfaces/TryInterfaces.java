62bdcd6d4c0db75fccb78c5f1418b5d3427901c6
ainonext <sm_sv_ma@mail.ru>
2013-03-15 00:15:14 +0400

    extention for interfaces

--- TryInterfaces/src/tryinterfaces/TryInterfaces.java
+++ TryInterfaces/src/tryinterfaces/TryInterfaces.java
@@ -11,7 +11,34 @@ public class TryInterfaces {
 
         int funB(int n);
     }
+    interface One{
+        void setOne(int n);
+    }
+    interface Two{
+        void setTwo(int n);
+    }
+     class ClassA{
+        int number;
+        void show(){
+            System.out.println("Поле number "+number);
+        }
+    }
+    static class ClassB extends ClassA implements One,Two{
+        int value;
+       public void setOne(int n) {
+            number=n;
+        }
 
+        
+        public void setTwo(int n) {
+            value=n;
+        }
+        void show(){
+            super.show();
+            System.out.println("Поле value "+value);
+        }
+    
+    }
     static class MyClass implements BaseB {
 
         public int funA(int n) {
@@ -35,5 +62,13 @@ public class TryInterfaces {
         MyClass obj = new MyClass();
         System.out.println("1 " + obj.funA(5));
         System.out.println("2 " + obj.funB(5));
+        One ref1;
+        Two ref2;
+        ClassB object = new ClassB();
+        ref1=object;
+        ref2=object;
+        ref1.setOne(10);
+        ref2.setTwo(-50);
+        object.show();
     }
 }
