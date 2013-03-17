4eafb6b3afbdc9b0eb232d56e511fc432d6532e9
StoryD <sm_sv_ma@mail.ru>
2013-03-17 23:22:01 +0300

    det for 1x1 and 2x2 matrix

--- Recursion/src/recursion/Recursion.java
+++ Recursion/src/recursion/Recursion.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class Recursion {
 
     static int mSuperPow(int num, int pow) {
@@ -21,14 +23,34 @@ public class Recursion {
     }
 
     public static void main(String[] args) {
-        int[][] matrix = new int [5][5];
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                matrix[i][j]=(int)(Math.random()*10*5);
-                
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите разморность матрицы ");
+        int m = in.nextInt();
+        int[][] matrix = new int[m][m];
+        int pr = 1, prRev = 1;
+        int det;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = (int) (Math.random() * 10 * 5);
+                System.out.print(matrix[i][j] + " ");
             }
+            System.out.println();
         }
+        if (m == 1) {
+            System.out.println("Определитель 1х1 " + matrix[0][0]);
+        }
+        if (m == 2) {
+            for (int i = 0; i < m; i++) {
+                pr = pr * matrix[i][i];
+                prRev = prRev * matrix[i][m - 1 - i];
+
+            }
+            det = pr - prRev;
+            System.out.println("Определитель 2х2 " + det);
+        }
+
         System.out.println("Number " + mSuperPow(2, 3));
         numbers(5);
+
     }
 }
