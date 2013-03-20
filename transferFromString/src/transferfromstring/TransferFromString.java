5a80d7b234c01f43753a0b2e7d7520a537bc0855
StoryD <sm_sv_ma@mail.ru>
2013-03-20 22:00:38 +0400

    string with letters before and after 1 and 0

--- transferFromString/src/transferfromstring/TransferFromString.java
+++ transferFromString/src/transferfromstring/TransferFromString.java
@@ -1,24 +1,34 @@
 package transferfromstring;
 
 public class TransferFromString {
-static public int numpow(int pow){
-    int num=2;
-    if (pow == 0) {
+
+    static public int numpow(int pow) {
+        int num = 2;
+        if (pow == 0) {
             return num = 1;
         }
-    pow--;
-    return numpow( pow) * num;
-}
+        pow--;
+        return numpow(pow) * num;
+    }
+
     public static void main(String[] args) {
-       String str="11111";
-       System.out.print("Строка с двоичным числом "+str+"\n");
-       int len=str.length()-1;
-       int res=0;
-      for(int i=0;i<=str.length()-1;i++,len--){
-           if(((int)str.charAt(i)-48)>0) {
-               res+=((int)str.charAt(i)-48)*numpow(len);
-           }
-       }
-       System.out.print("В десятичной системе "+res+"\n");
+        String str = "aba11111nk";
+        System.out.print("Строка с двоичным числом " + str + "\n");
+        int res = 0;
+        int tmp = -1;
+        for (int counter = 0; counter <= str.length() - 1; counter++) {
+            if (str.charAt(counter) == '1' || str.charAt(counter) == '0') {
+                tmp += 1;
+            }
+        }
+        for (int i = 0; i <= str.length() - 1; i++) {
+            if (str.charAt(i) == '1' || str.charAt(i) == '0') {
+                if (((int) str.charAt(i) - 48) > 0) {
+                    res += ((int) str.charAt(i) - 48) * numpow(tmp);
+                    tmp--;
+                }
+            }
+        }
+        System.out.print("В десятичной системе число " + res + "\n");
     }
 }
