1b214da1d51901671fededb4cc0035a5bdbeb7b5
StoryD <sm_sv_ma@mail.ru>
2013-03-22 00:31:54 +0400

    modified sort

--- Sorts/src/sorts/Sorts.java
+++ Sorts/src/sorts/Sorts.java
@@ -3,27 +3,30 @@ package sorts;
 public class Sorts {
 
     public static void main(String[] args) {
-        int[] array = new int[]{3, 2, 1, 5, 3};
+        int[] array = new int[]{1, 2, 4, 3};
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
         int len = array.length;
         int tmp;
-        for (int i = 0; i < len-1; i++) {
-            for (int j = 0; j < len-1; j++) {
+        int f;
+        for (int i = 0; i < len - 1; i++) {
+            f = 0;
+            for (int j = 0; j < (len - 1); j++) {
                 if (array[j] > array[j + 1]) {
                     tmp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = tmp;
-                    
+                    f = 1;
                 }
-                
+            }
+            if (f == 0) {
+                break;
             }
         }
         System.out.println();
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
-
     }
 }
