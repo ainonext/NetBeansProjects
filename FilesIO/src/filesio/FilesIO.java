4c148869bbcec9be60d1fd2c6fa0cbe9c0253476
StoryD <sm_sv_ma@mail.ru>
2013-04-01 00:01:42 +0400

    files_IO
    first try to read and write into file

---
+++ FilesIO/src/filesio/FilesIO.java
@@ -0,0 +1,27 @@
+package filesio;
+
+import java.io.*;
+
+public class FilesIO {
+
+    public static void main(String[] args) throws IOException {
+        int a;
+        try {
+            FileOutputStream cout = new FileOutputStream("C:/Users/Aiharo/Documents/NetBeansProjects/FilesIO/test.txt");
+            FileInputStream cin = new FileInputStream("C:/Users/Aiharo/Documents/NetBeansProjects/FilesIO/test1.txt");
+            a = cin.read();
+            while (a != -1) {
+                if (a == (int) ' ') {
+                    a = '_';
+                }
+                cout.write(a);
+                a = cin.read();
+            }
+            cout.close();
+            cin.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден!");
+            System.out.println("Завершение работы программы!");
+        }
+    }
+}
